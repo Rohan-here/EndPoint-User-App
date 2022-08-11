bd79ba93272927fdbedf2d27efaf5606ccb417a5
Rohan Gupta <guptarohan2002@gmail.com>
2022-08-11 17:58:56 +0530

    Port Updated

--- backend/app.js
+++ backend/app.js
@@ -21,5 +21,5 @@ app.get("/api/fetch", async (req, res) => {
   }
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT | 5000;
 app.listen(port, () => console.log("Listening on Port " + port));
