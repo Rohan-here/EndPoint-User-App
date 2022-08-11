2b1980a8d93d9ac7c6b003612f810a16460f320c
Rohan Gupta <guptarohan2002@gmail.com>
2022-08-11 15:53:48 +0530

    Backend Working

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv").config();
+const axios = require("axios");
+
+app.get("/api/fetch", async (req, res) => {
+  try {
+    const config = {
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/users",
+    };
+    const { data } = await axios(config);
+    res.status(200).json({ data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+});
+
+const port = process.env.PORT;
+app.listen(port, () => console.log("Listening on Port " + port));
